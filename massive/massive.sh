de851eaf10a87f4634df6ee1860ffd03667f0297
ssebbass <ssebbass@gmail.com>
2015-12-18 16:32:15 -0300

    First commit

--- massive/massive.sh
+++ massive/massive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# set -x
+#set -x
 
 # Check for basic tools
 which sshpass >/dev/null 2>&1 || echo "Please install sshpass"
@@ -16,17 +16,26 @@ HOSTLIST="$1"
 SCRIPT="$2"
 SSHUSR="$3"
 PASSWD="$4"                                                                     # This is optional
-SSHOPT="-o ConnectTimeout=5 -o ConnectionAttempts=2"                       # Some default ssh options
+SSHOPT="-t -o ConnectTimeout=5 -o ConnectionAttempts=2"                            # Some default ssh options
 DATE=$(date +%Y%m%d%H%M)
 LOGFILE="$0.$DATE.log"
 TMPFILE="$(mktemp)"
+TEXTDELIMETER="704e756a950d0d3d38193e013a3e4767"
 
 function clean {
   rm -f $TMPFILE >/dev/null 2>&1
 }
 
 function sshwithpasswd {
-  sshpass -p $PASSWD ssh $SSHOPT $SSHUSR@$HOST < "$SCRIPT" 2>/dev/null > tmpfile
+  for HOST in $( cat $HOSTLIST ); do
+    echo -e "\n   *** $HOST ***"
+    sshpass -p $PASSWD ssh $SSHOPT $SSHUSR@$HOST < "$SCRIPT" >$TMPFILE 2>&1 ; RETVAL="$?"
+    if [ "$RETVAL" = 0 ]; then
+      sed "0,/"$TEXTDELIMETER"/d" $TMPFILE
+    else
+      echo "Error on host $HOST"
+    fi
+  done
 }
 
 function sshwithoutpasswd {
---
+++ massive/sshscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# set -x
+
+TEXTDELIMETER="704e756a950d0d3d38193e013a3e4767"
+echo $TEXTDELIMETER
+
+############## Put your script after this line ##########
+
