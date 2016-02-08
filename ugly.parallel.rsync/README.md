84b9dd22a8de5ecf07798e67c92390a07be3a0ed
ssebbass <ssebbass@gmail.com>
2016-02-08 12:21:50 -0300

    Updated

---
+++ ugly.parallel.rsync/README.md
@@ -0,0 +1,1 @@
+Some ugly script to get rsync over ssh in parallel to many hosts
--- ugly.parallel.rsync/ugly.parallel.rsync.sh
+++ ugly.parallel.rsync/ugly.parallel.rsync.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -e
 
-LIMITE="5"
-SLEEP="10"
-RSYNC="rsync --delete -qPhae"
+if [ ! -f "$1" ]; then
+  echo Usage: $0 [hosts file]
+  exit 1
+else
+  LIST="$1"
+fi
 
-for HOST in $( cat lista ) ; do
+LIMITE="3"
+SLEEP="10"
+SSHOPT="ssh -F $HOME/.ssh/config -l root"
+RSYNC="rsync --delete -qzPhae"
+
+for HOST in $( cat $LIST ) ; do
   mkdir $HOST >/dev/null 2>&1 || true
 
   while true; do
@@ -16,10 +24,21 @@ for HOST in $( cat lista ) ; do
       sleep $SLEEP
     else
       echo lanzo rsync $HOST...
-      $RSYNC "ssh -l root" $HOST:/root/monitoreo/ $HOST/ &
+      # $RSYNC $HOST:/root/monitoreo/ $HOST:/etc/fstab $HOST:/etc/apache2 $HOST:/etc/sysctl.conf $HOST:/var/log $HOST/ &
+      $RSYNC "$SSHOPT" $HOST:/root/monitoreo/ $HOST:/etc/fstab $HOST:/etc/apache2 $HOST:/etc/sysctl.conf $HOST/ &
+      sleep 1
       break  
     fi
 
   done
-
 done
+
+while true; do
+  if [ "$( ps -ef | grep -v grep | grep "$RSYNC" )" != "" ]; then
+    echo -e ".\c"
+    sleep 1
+  else
+    exit 0
+  fi
+done
+  
