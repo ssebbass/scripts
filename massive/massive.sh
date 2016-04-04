6e981af7ea6d8827ca0af76f4ec56fb5bf3b3202
Juarez Sebastian <ssebbass@gmail.com>
2016-04-04 12:24:45 -0300

    Some static retouch

--- massive/massive.sh
+++ massive/massive.sh
@@ -3,8 +3,8 @@
 
 # Check for basic tools
 which sshpass >/dev/null 2>&1 || echo "Please install sshpass"
-which ssh >/dev/null 2>&1 || echo "Please install ssh client"
-which mktemp >/dev/null 2>&1 || echo "Missing mktemp"
+which ssh >/dev/null 2>&1     || echo "Please install ssh client"
+which mktemp >/dev/null 2>&1  || echo "Missing mktemp"
 
 # Check for files
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
@@ -16,8 +16,8 @@ HOSTLIST="$1"
 SCRIPT="$2"
 SSHUSR="$3"
 PASSWD="$4"                                                                     # This is optional
-SSHOPT="-F $HOME/.ssh/config -t -o ConnectTimeout=5 -o ConnectionAttempts=2"                         # Some default ssh options
-DATE=$(date +%Y%m%d%H%M)
+SSHOPT="-F $HOME/.ssh/config -t -o ConnectTimeout=5 -o ConnectionAttempts=2"    # Some default ssh options
+DATE="$( date +%Y%m%d%H%M )"
 WORKDIR="$( pwd )"
 LOGFILE="$WORKDIR/$(basename $0).$DATE.log"
 TMPFILE="$(mktemp)"
@@ -29,7 +29,7 @@ function clean {
 
 function sshwithpasswd {
   for HOST in $( cat $HOSTLIST ); do
-#    echo -e "\n   *** $HOST ***"
+    #    echo -e "\n   *** $HOST ***"
     echo haciendo ssh a $HOST
     sshpass -p $PASSWD ssh $SSHOPT $SSHUSR@$HOST < "$SCRIPT" >$TMPFILE 2>&1 ; RETVAL="$?"
     if [ "$RETVAL" = 0 ]; then
@@ -63,7 +63,5 @@ function main {
   fi
 }
 
-main | tee -a $LOGFILE 
+main | tee -a $LOGFILE
 clean
-
-
